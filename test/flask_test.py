b89db68c593aa909a1cf062ab3e8bfc5a1709a60
shao-chi <shaochi1225@gmail.com>
2020-02-18 18:03:59 +0800

    main_app_run

--- test/flask_test.py
+++ test/flask_test.py
@@ -5,5 +5,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-if __name__ == '__main__':
-    app.run()
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
