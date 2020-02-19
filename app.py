c34559501f8ceb6e6501aa1ec6f540d75aa1b400
shao-chi <shaochi1225@gmail.com>
2020-02-19 17:10:17 +0800

    dict_test

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 #app.py
 from flask import Flask, request, abort
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -11,10 +10,16 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+from linebot_function import en_dictionary
+
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('yizrFotfzhHrVUSlSC98/TBnMfiJRzXyZ43XWNYGgG+2JrXq8EZ+RsUoSc2Egpik/soa9dcfd5WgsiwuhQrGGzYqG5U1Mh8jarNP6/kCk93/NHBl2X2lkj3xx2ODKQ62r+TfiMU6xG2BqKdPWRUPOwdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('65050cc53fbb7ded70a1213824ae8894')
+line_bot_api = \
+    LineBotApi('yizrFotfzhHrVUSlSC98/TBnMfiJRzXyZ43XWNYGgG+2JrXq8EZ+RsUoSc2Egpik \
+        /soa9dcfd5WgsiwuhQrGGzYqG5U1Mh8jarNP6/kCk93/NHBl2X2lkj3xx2ODKQ62r+TfiMU6 \
+        xG2BqKdPWRUPOwdB04t89/1O/w1cDnyilFU=')
+handler = \
+    WebhookHandler('65050cc53fbb7ded70a1213824ae8894')
 
 
 @app.route("/callback", methods=['POST'])
@@ -37,10 +42,21 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    word = event.message.text
+    result = en_dictionary(word)
 
+    for res in result:
+        text = word + ' ({})'.format(res['part_of_speech'])
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=text))
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text='UK pronounciation'))
+        line_bot_api.reply_message(
+            event.reply_token, AudioSendMessage(original_content_url=res['uk_kk']))
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text='US pronounciation'))
+        line_bot_api.reply_message(
+            event.reply_token, AudioSendMessage(original_content_url=res['us_kk']))
 
 import os
 if __name__ == "__main__":
---
+++ linebot_function.py
@@ -0,0 +1,67 @@
+import requests
+from bs4 import BeautifulSoup
+
+# class crawl():
+#     """
+#     """
+#     def 
+
+# re.compile("")
+# def crawl(url, name, class_, id, href, string):
+#     req = requests.get(url)
+#     soup = BeautifulSoup(r.text,"html.parser")
+
+def en_dictionary(keyword):
+    """
+    return: list -> dict
+        'part_of_speech': string
+        'uk_kk': string
+        'us_kk': string
+        'uk_audio': string(url)
+        'us_audio': string(url)
+        'description': list -> dict
+            'guide_word': string
+            'def': list -> dict
+                'def_en': string
+                'def_tw': string
+                'sentences': list -> string
+    """
+    desktop = "https://dictionary.cambridge.org"
+    url = "https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/{}".format(keyword)
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text,"html.parser")
+
+    word = soup.find(name='span', class_='hw dhw').text
+    result = []
+    word_def = soup.find_all(name='div', class_='pr entry-body__el')
+    for define in word_def:
+        res = dict()
+        res['part_of_speech'] = define.find(name='span', class_='pos dpos').text
+        kk_pronounce = define.find_all(name='span', class_='ipa dipa lpr-2 lpl-1')
+        res['uk_kk'] = '/' + kk_pronounce[0].text + '/'
+        res['us_kk'] = '/' + kk_pronounce[1].text + '/'
+        audio = soup.find_all(name='source', type='audio/mpeg')
+        res['uk_audio'] = desktop + audio[0].get('src')
+        res['us_audio'] = desktop + audio[1].get('src')
+
+        res['description'] = list()
+        descripe = define.find_all(name='div', class_='pr dsense')
+        for description in descripe:
+            des = dict()
+            des['guide_word'] = description.find(name='span', class_='guideword dsense_gw').find('span').text
+        
+            des['def'] = list()
+            blocks = description.find_all(name='div', class_='def-block ddef_block')
+            for b in blocks:
+                def_block = dict()
+                def_block['def_en'] = b.find(name='div', class_='def ddef_d db').text
+                def_block['def_tw'] = b.find(name='span', class_='trans dtrans dtrans-se').text
+                def_block['sentences'] = [s.text for s in b.find_all(name='div', class_='examp dexamp')]
+
+                des['def'].append(def_block)
+
+            res['description'].append(des)
+
+        result.append(res)
+
+    return result
